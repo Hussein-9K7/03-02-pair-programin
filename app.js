bff66663f48867ec6df302f273d65cad0793f2f5
Hussein <alprstigehussein@gmail.com>
2025-02-02 20:17:45 +0200

    making new user is working

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const app = express();
 
+// استيراد الموجهات الخاصة بالمستخدمين
+const userRouter = require('./routes/userRouter'); 
+
+// استيراد الوظائف الخاصة بالجولات
 const {
   getAllTours,
   getTourById,
   createTour,
   updateTour,
   deleteTour,
-} = require("./tourHandlers.js"); 
+} = require("./controllers/tourControllers");
 
-// Middleware to parse JSON
+// Middleware لتحليل JSON
 app.use(express.json());
 
-// ROUTES
-// GET /tours
-app.get("/tours", getAllTours);
+// مسار المستخدمين
+app.use('/users', userRouter);
 
-// POST /tours
-app.post("/tours", createTour);
-
-// GET /tours/:tourId
-app.get("/tours/:tourId", getTourById);
-
-// PUT /tours/:tourId
-app.put("/tours/:tourId", updateTour);
-
-// DELETE /tours/:tourId
-app.delete("/tours/:tourId", deleteTour);
+// مسارات الجولات (Tours)
+app.get("/tours", getAllTours);  // GET /tours
+app.post("/tours", createTour);  // POST /tours
+app.get("/tours/:tourId", getTourById);  // GET /tours/:tourId
+app.put("/tours/:tourId", updateTour);  // PUT /tours/:tourId
+app.delete("/tours/:tourId", deleteTour);  // DELETE /tours/:tourId
 
 const port = 4000;
-// Start the server
+
+// بدء الخادم
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
