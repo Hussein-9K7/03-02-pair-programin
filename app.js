6781ff9cc2aa2c3e1191304257a7bd569ec5d7e2
Hussein <alprstigehussein@gmail.com>
2025-02-02 22:29:10 +0200

    step 5 is done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');  
 
-// استيراد الموجهات الخاصة بالمستخدمين
 const userRouter = require('./routes/userRouter'); 
 const tourRouter = require('./routes/tourRouter');
-// استيراد الوظائف الخاصة بالجولات
+
+
 const {
   getAllTours,
   getTourById,
@@ -13,18 +14,22 @@ const {
   deleteTour,
 } = require("./controllers/tourControllers");
 
-// Middleware لتحليل JSON
+
 app.use(express.json());
+
+
+app.use(morgan('tiny'));  
+
 
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
-// مسارات الجولات (Tours)
-app.get("/tours", getAllTours);  // GET /tours
-app.post("/tours", createTour);  // POST /tours
-app.get("/tours/:tourId", getTourById);  // GET /tours/:tourId
-app.put("/tours/:tourId", updateTour);  // PUT /tours/:tourId
-app.delete("/tours/:tourId", deleteTour);  // DELETE /tours/:tourId
+
+app.get("/tours", getAllTours);  
+app.post("/tours", createTour);  
+app.get("/tours/:tourId", getTourById); 
+app.put("/tours/:tourId", updateTour);  
+app.delete("/tours/:tourId", deleteTour); 
 
 const port = 4000;
 
